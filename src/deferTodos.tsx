46f01b55213b6f5128ed3067e1a032ebb340e7ba
David Vargas <dvargas92495@gmail.com>
2022-07-20 15:20:26 -0400

    register button click

--- src/deferTodos.tsx
+++ src/deferTodos.tsx
@@ -10,6 +10,7 @@ import createOverlayRender from "roamjs-components/util/createOverlayRender";
 import { useState, useCallback, useRef, useEffect } from "react";
 import { Alert, Classes, InputGroup, Label, H3 } from "@blueprintjs/core";
 import updateBlock from "roamjs-components/writes/updateBlock";
+import getBlockUidFromTarget from "roamjs-components/dom/getBlockUidFromTarget";
 
 type Props = {
   resolve: (value: string) => void;
@@ -70,175 +71,177 @@ ny = next year`}
   );
 };
 
+const deferTodos = (blockUid: string) => {
+  if (!blockUid) return;
+  const blockText = getTextByBlockUid(blockUid);
+  const varDefDate = blockText.match(/varDefDate=([^}]+)}/)?.[1];
+  const varTodayDNP = window.roamAlphaAPI.util.dateToPageTitle(new Date());
+  var foundButton = blockText.indexOf("(Deferrals:") > -1;
+  var rmDateFormatStr = typeof varDefDate === "undefined" ? "" : varDefDate;
+  var usingDNP = false;
+  if (!foundButton) {
+    //No button found which means no date variable set so need to parse out a DNP date
+    //Find the last date in the block
+    var arrDnp = blockText.split(
+      /(\[\[[^\]]*?\s[0-9]+(?:st|nd|rd|th),\s[0-9]{4}\]\])/
+    );
+    if (arrDnp.length > 1) {
+      var lastDate = arrDnp[arrDnp.length - 2];
+    } else {
+      var lastDate = varTodayDNP;
+      usingDNP = true;
+    }
+    rmDateFormatStr = lastDate
+      .replace("[[", "")
+      .replace("]]", "")
+      .replace("st,", "")
+      .replace("rd,", "")
+      .replace("th,", "")
+      .replace("nd,", "");
+  }
+
+  //Set some variables
+  var btnCounter = 0;
+  var finalString = "";
+  var rmDateParse = new Date(Date.parse(rmDateFormatStr));
+  new Promise<string>((resolve) =>
+    createOverlayRender<Props>("defer-todos-prompt", Prompt)({ resolve })
+  ).then((howManyDays) => {
+    var howManyDaysInt = parseInt(howManyDays);
+
+    if (isNaN(howManyDaysInt)) {
+      //Random dates for this week, next week, etc.
+      switch (howManyDays) {
+        //Sunday = 0, Saturday = 6
+        case "tw":
+          var curDayOfWeek = rmDateParse.getDay();
+          if (curDayOfWeek == 6) {
+            curDayOfWeek = 0;
+          }
+          howManyDaysInt =
+            1 + Math.floor(Math.random() * Math.floor(6 - curDayOfWeek));
+          break;
+        case "nw":
+          var curDayOfWeek = rmDateParse.getDay();
+          howManyDaysInt =
+            6 - curDayOfWeek + 1 + Math.floor(Math.random() * Math.floor(7));
+          break;
+        case "tm":
+          var curDayOfMonth = rmDateParse.getDate();
+          var endOfMonthDate = endOfMonth(rmDateParse).getDate();
+          var endOfNextMonth = endOfMonth(addMonths(rmDateParse, 1)).getDate();
+          if (curDayOfMonth == endOfMonthDate) {
+            curDayOfMonth = 1;
+            endOfMonthDate = endOfNextMonth;
+          }
+          howManyDaysInt =
+            1 +
+            Math.floor(
+              Math.random() * Math.floor(endOfMonthDate - curDayOfMonth)
+            );
+          break;
+        case "nm":
+          var curDayOfMonth = rmDateParse.getDate();
+          var endOfMonthDate = endOfMonth(rmDateParse).getDate();
+          var endOfNextMonth = endOfMonth(addMonths(rmDateParse, 1)).getDate();
+          howManyDaysInt =
+            endOfMonthDate -
+            curDayOfMonth +
+            1 +
+            Math.floor(Math.random() * Math.floor(endOfNextMonth));
+          break;
+        case "ty":
+          var daysLeftInYear = differenceInDays(
+            endOfYear(rmDateParse),
+            new Date()
+          );
+          howManyDaysInt =
+            1 + Math.floor(Math.random() * Math.floor(daysLeftInYear));
+          break;
+        case "ny":
+          var daysLeftInYear = differenceInDays(
+            endOfYear(rmDateParse),
+            new Date()
+          );
+          howManyDaysInt =
+            1 + daysLeftInYear + Math.floor(Math.random() * Math.floor(365));
+          break;
+        default:
+          howManyDaysInt = 1;
+          break;
+      }
+    }
+
+    var nextDate = new Date(
+      rmDateParse.getFullYear(),
+      rmDateParse.getMonth(),
+      rmDateParse.getDate() + howManyDaysInt
+    );
+    var rmDateFormat = "[[" + dateFnsFormat(nextDate, "MMMM do, yyyy") + "]]";
+    rmDateFormatStr = rmDateFormat
+      .replace("[[", "")
+      .replace("]]", "")
+      .replace(/(st|nd|rd|th)?, /, " ");
+    if (blockText.split(/(\(Deferrals:.*\))/).length > 1) {
+      var foundNum = parseInt(
+        blockText
+          .split(/(\(Deferrals:.*\))/)[1]
+          .replace("(Deferrals:", "")
+          .replace(")", "")
+          .trim()
+      );
+      if (Number.isInteger(foundNum)) {
+        btnCounter = foundNum;
+      }
+      btnCounter++;
+      finalString =
+        rmDateFormat +
+        " (Deferrals: " +
+        btnCounter +
+        ") {{Defer:varDefDate=" +
+        rmDateFormatStr +
+        "}}";
+    } else {
+      btnCounter++;
+      if (usingDNP) {
+        finalString = varTodayDNP + " ";
+      }
+      finalString =
+        finalString +
+        "**Deferred To:** " +
+        rmDateFormat +
+        " (Deferrals: " +
+        btnCounter +
+        ") {{Defer:varDefDate=" +
+        rmDateFormatStr +
+        "}}";
+    }
+
+    updateBlock({
+      uid: blockUid,
+      text: `${blockText.replace(
+        /\[\[[a-zA-Z]+ \d{1\,2}[sthndr]{2}\, \d{4}\]\] \(Deferrals:.*$/,
+        ""
+      )} ${finalString}`,
+    });
+  });
+};
+
 const addDeferTODOsCommand = () => {
   window.roamAlphaAPI.ui.commandPalette.addCommand({
     label: "Defer TODO",
     callback: () => {
       const blockUid = window.roamAlphaAPI.ui.getFocusedBlock()["block-uid"];
-      if (!blockUid) return;
-      const blockText = getTextByBlockUid(blockUid);
-      const varDefDate = blockText.match(/varDefDate=([^}]+)}/)?.[1];
-      const varTodayDNP = window.roamAlphaAPI.util.dateToPageTitle(new Date());
-      var foundButton = blockText.indexOf("(Deferrals:") > -1;
-      var rmDateFormatStr = typeof varDefDate === "undefined" ? "" : varDefDate;
-      var usingDNP = false;
-      if (!foundButton) {
-        //No button found which means no date variable set so need to parse out a DNP date
-        //Find the last date in the block
-        var arrDnp = blockText.split(
-          /(\[\[[^\]]*?\s[0-9]+(?:st|nd|rd|th),\s[0-9]{4}\]\])/
-        );
-        if (arrDnp.length > 1) {
-          var lastDate = arrDnp[arrDnp.length - 2];
-        } else {
-          var lastDate = varTodayDNP;
-          usingDNP = true;
-        }
-        rmDateFormatStr = lastDate
-          .replace("[[", "")
-          .replace("]]", "")
-          .replace("st,", "")
-          .replace("rd,", "")
-          .replace("th,", "")
-          .replace("nd,", "");
-      }
-
-      //Set some variables
-      var btnCounter = 0;
-      var finalString = "";
-      var rmDateParse = new Date(Date.parse(rmDateFormatStr));
-      new Promise<string>((resolve) =>
-        createOverlayRender<Props>("defer-todos-prompt", Prompt)({ resolve })
-      ).then((howManyDays) => {
-        var howManyDaysInt = parseInt(howManyDays);
-
-        if (isNaN(howManyDaysInt)) {
-          //Random dates for this week, next week, etc.
-          switch (howManyDays) {
-            //Sunday = 0, Saturday = 6
-            case "tw":
-              var curDayOfWeek = rmDateParse.getDay();
-              if (curDayOfWeek == 6) {
-                curDayOfWeek = 0;
-              }
-              howManyDaysInt =
-                1 + Math.floor(Math.random() * Math.floor(6 - curDayOfWeek));
-              break;
-            case "nw":
-              var curDayOfWeek = rmDateParse.getDay();
-              howManyDaysInt =
-                6 -
-                curDayOfWeek +
-                1 +
-                Math.floor(Math.random() * Math.floor(7));
-              break;
-            case "tm":
-              var curDayOfMonth = rmDateParse.getDate();
-              var endOfMonthDate = endOfMonth(rmDateParse).getDate();
-              var endOfNextMonth = endOfMonth(
-                addMonths(rmDateParse, 1)
-              ).getDate();
-              if (curDayOfMonth == endOfMonthDate) {
-                curDayOfMonth = 1;
-                endOfMonthDate = endOfNextMonth;
-              }
-              howManyDaysInt =
-                1 +
-                Math.floor(
-                  Math.random() * Math.floor(endOfMonthDate - curDayOfMonth)
-                );
-              break;
-            case "nm":
-              var curDayOfMonth = rmDateParse.getDate();
-              var endOfMonthDate = endOfMonth(rmDateParse).getDate();
-              var endOfNextMonth = endOfMonth(
-                addMonths(rmDateParse, 1)
-              ).getDate();
-              howManyDaysInt =
-                endOfMonthDate -
-                curDayOfMonth +
-                1 +
-                Math.floor(Math.random() * Math.floor(endOfNextMonth));
-              break;
-            case "ty":
-              var daysLeftInYear = differenceInDays(
-                endOfYear(rmDateParse),
-                new Date()
-              );
-              howManyDaysInt =
-                1 + Math.floor(Math.random() * Math.floor(daysLeftInYear));
-              break;
-            case "ny":
-              var daysLeftInYear = differenceInDays(
-                endOfYear(rmDateParse),
-                new Date()
-              );
-              howManyDaysInt =
-                1 +
-                daysLeftInYear +
-                Math.floor(Math.random() * Math.floor(365));
-              break;
-            default:
-              howManyDaysInt = 1;
-              break;
-          }
-        }
-
-        var nextDate = new Date(
-          rmDateParse.getFullYear(),
-          rmDateParse.getMonth(),
-          rmDateParse.getDate() + howManyDaysInt
-        );
-        var rmDateFormat =
-          "[[" + dateFnsFormat(nextDate, "MMMM do, yyyy") + "]]";
-        rmDateFormatStr = rmDateFormat
-          .replace("[[", "")
-          .replace("]]", "")
-          .replace(/(st|nd|rd|th)?, /, " ");
-        if (blockText.split(/(\(Deferrals:.*\))/).length > 1) {
-          var foundNum = parseInt(
-            blockText
-              .split(/(\(Deferrals:.*\))/)[1]
-              .replace("(Deferrals:", "")
-              .replace(")", "")
-              .trim()
-          );
-          if (Number.isInteger(foundNum)) {
-            btnCounter = foundNum;
-          }
-          btnCounter++;
-          finalString =
-            rmDateFormat +
-            " (Deferrals: " +
-            btnCounter +
-            ") {{Defer:varDefDate=" +
-            rmDateFormatStr +
-            "}}";
-        } else {
-          btnCounter++;
-          if (usingDNP) {
-            finalString = varTodayDNP + " ";
-          }
-          finalString =
-            finalString +
-            "**Deferred To:** " +
-            rmDateFormat +
-            " (Deferrals: " +
-            btnCounter +
-            ") {{Defer:varDefDate=" +
-            rmDateFormatStr +
-            "}}";
-        }
-
-        updateBlock({
-          uid: blockUid,
-          text: `${blockText.replace(
-            /\[\[[a-zA-Z]+ \d{1\,2}[sthndr]{2}\, \d{4}\]\] \(Deferrals:.*$/,
-            ""
-          )} ${finalString}`,
-        });
-      });
+      deferTodos(blockUid);
     },
+  });
+
+  document.addEventListener("click", (e) => {
+    const target = e.target as HTMLElement;
+    if (target.nodeName === "BUTTON" && target.innerText === "Defer") {
+      const blockUid = getBlockUidFromTarget(target);
+      deferTodos(blockUid);
+    }
   });
 };
 
